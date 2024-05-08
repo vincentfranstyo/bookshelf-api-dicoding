c29bd93b4895713035e78f05b026184345fe6dc4
Vincent Franstyo <sorakiriko28814@gmail.com>
2024-05-09 01:50:04 +0700

    feat(CRUD): update and delete API

---
+++ src/routes/deleteBookById.js
@@ -0,0 +1,36 @@
+const { bookshelfData } = require("../data");
+const { bookshelfResponse } = require("./response");
+
+const deleteBookByIdHandler = (req, h) => {
+    const { id } = req.params;
+
+    const book = [...bookshelfData].find(book => book.id === id);
+
+    if (!book) {
+			return bookshelfResponse(
+				h,
+				undefined,
+				false,
+				"Buku gagal dihapus. Id tidak ditemukan",
+				404
+			);
+		}
+
+		bookshelfData.splice(bookshelfData.indexOf(book), 1);
+
+		return bookshelfResponse(
+			h,
+			undefined,
+			true,
+			"Buku berhasil dihapus",
+			200
+		);
+}
+
+const deleteBookById = {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler
+}
+
+module.exports = deleteBookById;
---
+++ src/routes/updateBookById.js
@@ -0,0 +1,76 @@
+const {bookshelfData} = require('../data');
+const {bookshelfResponse} = require('./response')
+
+const updateBookByIdHandler = (req, h) => {
+    const {id} = req.params;
+
+    const {
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading,
+    } = req.payload;
+
+    const book = [...bookshelfData].find((book) => book.id === id);
+
+    if (!name || name.trim() === "") {
+        return bookshelfResponse(h, undefined, false, "Gagal memperbarui buku. Mohon isi nama buku", 400);
+    }
+
+    if (readPage > pageCount) {
+			return bookshelfResponse(
+				h,
+				undefined,
+				false,
+				"Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
+				400
+			);
+		}
+
+		if (!book) {
+			return bookshelfResponse(
+				h,
+				undefined,
+				false,
+				"Gagal memperbarui buku. Id tidak ditemukan",
+				404
+			);
+		}
+
+		const updatedBook = {
+			id,
+			name,
+			year,
+			author,
+			summary,
+			publisher,
+			pageCount,
+			readPage,
+			reading,
+			finished: readPage === pageCount,
+			insertedAt: book.insertedAt,
+			updatedAt: new Date().toISOString(),
+		};
+
+		bookshelfData[bookshelfData.indexOf(book)] = updatedBook;
+
+		return bookshelfResponse(
+			h,
+			undefined,
+			true,
+			"Buku berhasil diperbarui",
+			200
+		);
+}
+
+const updateBookById = {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: updateBookByIdHandler
+}
+
+module.exports = updateBookById
